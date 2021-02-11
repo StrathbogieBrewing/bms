514cd156764b9f5c9fe7a216a7e48ea0d98137cd
Strathbogie Brewing Company <66310030+StrathbogieBrewing@users.noreply.github.com>
2021-02-12 07:36:36 +1100

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -19,5 +19,8 @@ avrdude
 # nohup socat /dev/ttyUSB0,echo=0,b9600 UDP4-DATAGRAM:192.168.8.255:12345,broadcast </dev/null >/dev/null 2>&1 &
 EOT
 
+# monitor network with
+# socat UDP4-RECVFROM:12345,broadcast,fork -
+
 # start data logging with ./datalog -d
 # start web server with busybox httpd -p 8080
